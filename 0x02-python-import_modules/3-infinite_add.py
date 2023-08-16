0f9fed71de5e828397b509041dd7a80890663fcd
Jamaalolaar <jamaalolaar@gmail.com>
2023-08-16 08:46:51 -0700

    fourth task

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print the sum of all the arguments"""
+
+    import sys
+
+    sum = 0;
+
+    for i in range(len(sys.argv) - 1):
+        sum += int(sys.argv[i + 1])
+        print("{}".format(sum))
